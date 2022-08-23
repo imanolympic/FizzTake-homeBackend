68b5507046a15ef188a883bd106bfdebf97d1c57
Theron Imanol Mansilla <66397972+imanolympic@users.noreply.github.com>
2022-08-23 10:54:57 -0400

    configured middleware routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import connect from "./data-access/index";
-import transactionsRouter from "./routers/transactions";
-import repaymentsRouter from "./routers/repayments";
+import transactionsRouter from "./routers/transactions.router";
+import repaymentsRouter from "./routers/repayments.router";
+import spendinglimitsRouter from "./routers/spendinglimits.router";
+
 import notFound from "./controllers/not-found";
 
 dotenv.config();
@@ -11,13 +12,12 @@ const app = express();
 app.use(express.json());
 app.use("/transactions", transactionsRouter);
 app.use("/repayments", repaymentsRouter);
-app.use("*", (req: Request, res: Response) => res.json(notFound()));
+app.use("/spendinglimits", spendinglimitsRouter);
+app.use("*", (_: Request, res: Response) => res.json(notFound()));
 
 const port = process.env.PORT || 5000;
-app.listen(port, async () => {
+app.listen(port, () => {
   console.log("listening on port:", port);
-
-  await connect();
 });
 
 export default app;
